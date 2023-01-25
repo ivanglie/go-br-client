5b622468d00ac2573c7f6251c754815a764bf364
ivanglie <ivanglie22@gmail.com>
2023-01-25 12:32:19 +0300

    Add some tests

--- br_test.go
+++ br_test.go
@@ -15,7 +15,30 @@ func Test_newBranch(t *testing.T) {
 	}
 }
 
-func Test_BySellSorter(t *testing.T) {
+func TestBranch_ByBuySorter(t *testing.T) {
+	got := []Branch{
+		newBranch("bank", "address", "subway", "currency", 101.34, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+		newBranch("bank", "address", "subway", "currency", 100.23, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+		newBranch("bank", "address", "subway", "currency", 56.78, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+		newBranch("bank", "address", "subway", "currency", 90.12, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+		newBranch("bank", "address", "subway", "currency", 12.34, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+	}
+	sort.Sort(ByBuySorter(got))
+
+	want := []Branch{
+		newBranch("bank", "address", "subway", "currency", 12.34, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+		newBranch("bank", "address", "subway", "currency", 56.78, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+		newBranch("bank", "address", "subway", "currency", 90.12, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+		newBranch("bank", "address", "subway", "currency", 100.23, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+		newBranch("bank", "address", "subway", "currency", 101.34, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("branch = %v, want %v", got, want)
+	}
+}
+
+func TestBranch_BySellSorter(t *testing.T) {
 	got := []Branch{
 		newBranch("bank", "address", "subway", "currency", 12.34, 58.78, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
 		newBranch("bank", "address", "subway", "currency", 12.34, 56.75, func() time.Time { t, _ := time.Parse("02.01.2006 15:04", "01.02.2018 12:35"); return t }()),
@@ -35,5 +58,30 @@ func Test_BySellSorter(t *testing.T) {
 
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("branch = %v, want %v", got, want)
+	}
+}
+
+func TestRates_String(t *testing.T) {
+	loc, _ := time.LoadLocation("Europe/Moscow")
+	b := newBranch(
+		"Банк «Открытие»",
+		"630102, г. Новосибирск, ул. Кирова, дом. 44",
+		"м. Октябрьская", "CNY",
+		9.61,
+		11.64,
+		time.Date(2023, time.January, 24, 16, 54, 0, 0, loc))
+
+	r := &Rates{}
+	r.Currency = CNY
+	r.City = Novosibirsk
+	r.Branches = []Branch{b}
+
+	got := r.String()
+	want := `{"currency":"CNY","city":"novosibirsk","branches":[{` +
+		`"bank":"Банк «Открытие»","address":"630102, г. Новосибирск, ул. Кирова, дом. 44","subway":"м. Октябрьская",` +
+		`"currency":"CNY","buy":9.61,"sell":11.64,"updated":"2023-01-24T16:54:00+03:00"}]}`
+
+	if got != want {
+		t.Errorf("got= %v, want= %v", got, want)
 	}
 }
