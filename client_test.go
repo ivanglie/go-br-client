44bc928806345203e8265316e9640376f21feefb
ivanglie <ivanglie22@gmail.com>
2023-05-19 00:05:56 +0300

    Add debug mode for log event, cleanup, add some tests (#4)

--- client_test.go
+++ client_test.go
@@ -1,16 +1,20 @@
 package br
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 )
 
 func TestClient_Rates(t *testing.T) {
-	dir, _ := os.Getwd()
-
 	c := NewClient()
-	c.url = "file:" + filepath.Join(dir, "/test/bankiru")
+	c.url.buildFunc = func() string {
+		dir, _ := os.Getwd()
+		return "file:" + filepath.Join(dir, "/test/bankiru")
+	}
+
 	r, err := c.Rates("", "")
 	if err != nil {
 		t.Error(err)
@@ -21,19 +25,33 @@ func TestClient_Rates(t *testing.T) {
 		t.Error("b is empty")
 	}
 
-	bCount := len(b)
-	if bCount != 5 {
+	if bCount := len(b); bCount != 5 {
 		t.Errorf("bCount got = %v, want %v", bCount, 5)
 	}
 }
 
 func TestClient_RatesError(t *testing.T) {
-	dir, _ := os.Getwd()
+	Debug = true
 
 	c := NewClient()
-	c.url = "file:" + filepath.Join(dir, "/test/invalid-bankiru")
-	_, err := c.Rates("", "")
-	if err == nil {
+	c.url.buildFunc = func() string {
+		dir, _ := os.Getwd()
+		return "file:" + filepath.Join(dir, "/test/invalid-bankiru")
+	}
+
+	if _, err := c.Rates(CNY, Sochi); err == nil {
 		t.Error(err)
+	}
+}
+
+func TestURL_build(t *testing.T) {
+	url := &URL{buildFunc: func() string {
+		return fmt.Sprintf(baseURL, strings.ToLower(string(Crnc)), Ct)
+	}}
+
+	want := (NewClient()).url.build()
+
+	if got := url.build(); got != want {
+		t.Errorf("URL.build() = %v, want %v", got, want)
 	}
 }
